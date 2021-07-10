23dde2e169954f18b575f3b746533e7d2ce515a9
Dawid.Rym <dawid.jscript@gmail.com>
2021-07-10 13:01:46 +0200

    added configuration of openfeign

--- src/main/java/com/customers/CustomersApplication.java
+++ src/main/java/com/customers/CustomersApplication.java
@@ -2,8 +2,12 @@ package com.customers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 public class CustomersApplication {
 
     public static void main(String[] args) {
